709d62d4ea10efcbcf1919eaa08af6eaf757d731
Sai Prakash JHUGROO <saih@elcaNet.local>
2024-03-12 13:17:54 +0400

    #main

--- src/components/background.tsx
+++ src/components/background.tsx
@@ -4,7 +4,7 @@ export function WavyBackgroundDemo() {
     return (
         <WavyBackground className="max-w-4xl mx-auto pb-40">
             <p className="text-2xl md:text-4xl lg:text-7xl text-white font-bold inter-var text-center">
-                Jiiya&apos;s Basement
+                endless_mru
             </p>
             <p className="text-base md:text-lg mt-4 text-white font-normal inter-var text-center"> <svg
                 xmlns="http://www.w3.org/2000/svg"
