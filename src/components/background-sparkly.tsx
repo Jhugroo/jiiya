3b748123b50bec31c8524fb6179851b5501eb541
Sai Prakash JHUGROO <saih@elcaNet.local>
2024-03-12 13:04:55 +0400

    #main

--- src/components/background-sparkly.tsx
+++ src/components/background-sparkly.tsx
@@ -5,8 +5,7 @@ export function SparklesPreview() {
 
     return (
         <div className="h-[40rem] w-full bg-black flex flex-col items-center justify-center overflow-hidden rounded-md">
-            <h1 className="md:text-7xl text-3xl lg:text-9xl font-bold text-center text-white relative z-20">
-            </h1>
+
             <MeteorsDemo />
             {/* Gradients */}
             <div className="absolute inset-x-20 top-0 bg-gradient-to-r from-transparent via-indigo-500 to-transparent h-[2px] w-3/4 blur-sm" />
@@ -23,8 +22,6 @@ export function SparklesPreview() {
                 particleColor="#FFFFFF"
             />
 
-            {/* Radial Gradient to prevent sharp edges */}
-            <div className="absolute inset-0 w-full h-full bg-black [mask-image:radial-gradient(350px_200px_at_top,transparent_20%,white)]"></div>
         </div>
     );
 }
